1e59486d82d2b3bc1a6c6a7a5e455b711a6eb850
Luiz Fernando <e.luizfernando2006@gmail.com>
2026-04-24 00:58:48 -0300

    Update and rename ProjetoYoutube.java to Main.java

--- ProjetoYoutube/src/projetoyoutube/Main.java
+++ ProjetoYoutube/src/projetoyoutube/Main.java
@@ -1,6 +1,6 @@
 package projetoyoutube;
 
-public class ProjetoYoutube {
+public class Main {
     public static void main(String[] args) {
         Usuario[] usuarios = new Usuario[2];
         Video[] videos = new Video[2];
